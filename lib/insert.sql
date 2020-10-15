57410dde548570da4f3aa96cac45c3a397da1bec
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-10-15 13:59:59 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 15, 'M', 'Brown', 'Chill', 1),
+('Rowdy', 20, 'M', 'Black', 'Not Chill', 1),
+('Tabitha', 17, 'F', 'Brown', 'Chill', 0),
+('Sergeant Brown', 30, 'M', 'Brown', 'Not Chill', 1),
+('Melissa', 4, 'F', 'Black', 'Super Chill', 1),
+('Grinch', 16, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 5, 'F', 'Brown', 'Chill', 1),
+(NULL, 30, 'M', 'Black', 'Chill', 0)
+
